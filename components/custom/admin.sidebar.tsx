f8a3eaf7ba5f79c50c878f764046fa38ae807b3d
PHILIPPE\phili <philippetan99@gmail.com>
2026-03-18 00:40:12 +0800

    login implemented and routes are protected

--- components/custom/admin.sidebar.tsx
+++ components/custom/admin.sidebar.tsx
@@ -1,12 +1,34 @@
 "use client";
 
 import { Button } from "../ui/button";
-import { LayoutDashboard, Users } from "lucide-react";
+import {
+  Box,
+  Building,
+  ChartNoAxesCombined,
+  CircleDollarSign,
+  ClipboardList,
+  Handbag,
+  LayoutDashboard,
+  LogOut,
+  ScrollText,
+  ShieldUser,
+  StickyNote,
+  Users,
+} from "lucide-react";
 import { Label } from "../ui/label";
-import { usePathname } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import { signOut } from "firebase/auth";
+import { auth } from "@/config/FirebaseConfig";
 
 const Sidebar = () => {
+  const router = useRouter();
   const pathname = usePathname();
+
+  const handleLogout = async () => {
+    await signOut(auth);
+    document.cookie = "session=; path=/; max-age=0";
+    router.push("/login");
+  };
 
   return (
     <div className="flex flex-col w-52 space-y-10">
@@ -18,6 +40,7 @@ const Sidebar = () => {
         <Button
           variant={pathname.includes("/dashboard") ? "default" : "ghost"}
           className="cursor-pointer w-full justify-start"
+          onClick={() => router.push("/admin/dashboard")}
         >
           <LayoutDashboard /> Dashboard
         </Button>
@@ -25,8 +48,83 @@ const Sidebar = () => {
         <Button
           variant={pathname.includes("/clients") ? "default" : "ghost"}
           className="cursor-pointer w-full justify-start"
+          onClick={() => router.push("/admin/clients")}
         >
           <Users /> Clients
+        </Button>
+
+        <Button
+          variant={pathname.includes("/projects") ? "default" : "ghost"}
+          className="cursor-pointer w-full justify-start"
+          onClick={() => router.push("/admin/projects")}
+        >
+          <ClipboardList /> Projects
+        </Button>
+
+        <Button
+          variant={pathname.includes("/inventory") ? "default" : "ghost"}
+          className="cursor-pointer w-full justify-start"
+          onClick={() => router.push("/admin/inventory")}
+        >
+          <Box /> Inventory
+        </Button>
+
+        <Button
+          variant={pathname.includes("/purchases") ? "default" : "ghost"}
+          className="cursor-pointer w-full justify-start"
+          onClick={() => router.push("/admin/purchases")}
+        >
+          <Handbag /> Purchases
+        </Button>
+
+        <Button
+          variant={pathname.includes("/expenses") ? "default" : "ghost"}
+          className="cursor-pointer w-full justify-start"
+          onClick={() => router.push("/admin/expenses")}
+        >
+          <CircleDollarSign /> Expenses
+        </Button>
+
+        <Button
+          variant={pathname.includes("/sales") ? "default" : "ghost"}
+          className="cursor-pointer w-full justify-start"
+          onClick={() => router.push("/admin/sales")}
+        >
+          <ChartNoAxesCombined /> Sales
+        </Button>
+
+        <Button
+          variant={pathname.includes("/suppliers") ? "default" : "ghost"}
+          className="cursor-pointer w-full justify-start"
+          onClick={() => router.push("/admin/suppliers")}
+        >
+          <Building /> Suppliers
+        </Button>
+
+        <Button
+          variant={pathname.includes("/reports") ? "default" : "ghost"}
+          className="cursor-pointer w-full justify-start"
+          onClick={() => router.push("/admin/reports")}
+        >
+          <ScrollText /> Reports
+        </Button>
+
+        <Button
+          variant={pathname.includes("/admins") ? "default" : "ghost"}
+          className="cursor-pointer w-full justify-start"
+          onClick={() => router.push("/admin/admins")}
+        >
+          <ShieldUser /> Admins
+        </Button>
+      </div>
+
+      <div className="w-full">
+        <Button
+          variant="outline"
+          className="w-full justify-start cursor-pointer"
+          onClick={handleLogout}
+        >
+          Logout <LogOut />
         </Button>
       </div>
     </div>
